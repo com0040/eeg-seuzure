b51a3a2d3a9da163bcf033b5f0fd3a3d3df1b23f
com0040 <103908794+com0040@users.noreply.github.com>
2022-07-07 15:31:56 +0900

    Add files via upload

---
+++ rader_pid/edf_npy(0707).py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 19:09:04 2022
+
+@author: chang
+"""
+
+import os # 파일을 복사하거나 디렉터리를 생성하고 특정 디렉터리 내의 파일 목록 구하기
+import csv
+import pandas as pd # 행과 열로 구성된 객체 생성, 안정적 데이터 처리
+ # import matplotlib.pyplot as plt # 시각화 패키지
+import numpy as np # 고성능 과학 계산용 패키지
+import natsort
+import sys
+from pandas import DataFrame
+import datetime as dt # from pidlist import pidlist # pidlist 파일에서 함수 전부 가져오기
+from tqdm import tqdm
+
+import pyedflib
+
+file_name = pyedflib.data.get_generator_filename()
+f = pyedflib.EdfReader(file_name)
+n = f.signals_in_file
+signal_labels = f.getSignalLabels()
+sigbufs = np.zeros((n, f.getNSamples()[0]))
+for i in np.arange(n):
+        sigbufs[i, :] = f.readSignal(i)
